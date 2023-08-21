f36790d762ee9943ece231227dda05bec9d3b940
psteph29 <paigestephenson29@gmail.com>
2023-08-21 09:58:32 -0600

    Initial Commit

---
+++ FitCheckPersonalCapstone/FitCheckPersonalCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitCheckPersonalCapstoneApp.swift
+//  FitCheckPersonalCapstone
+//
+//  Created by Paige Stephenson on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct FitCheckPersonalCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
